532ddef6af55649808ad89ded45aba6eef800644
antoniopd1 <antoniocan491@gmail.com>
2024-07-31 11:45:04 -0600

    Practica #2 Express

--- practica1-backend/myapp/app.js
+++ practica1-backend/myapp/app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
-app.get('/hello', (req, res) => {
-  const { name, lastName } = req.query;
-  res.send(`Hola ${name} ${lastName}!`);
+// Ruta 'users'
+app.route('/users')
+  .get((req, res) => {
+    res.send('GET: Se ha realizado una solicitud GET a la ruta /users');
+  })
+  .post((req, res) => {
+    res.send('POST: Se ha realizado una solicitud POST a la ruta /users');
+  })
+  .put((req, res) => {
+    res.send('PUT: Se ha realizado una solicitud PUT a la ruta /users');
+  })
+  .delete((req, res) => {
+    res.send('DELETE: Se ha realizado una solicitud DELETE a la ruta /users');
+  });
+
+// Manejo de errores 404
+app.use((req, res) => {
+  res.status(404).send('Error 404: La ruta solicitada no existe');
 });
 
 app.listen(port, () => {
