5dbbbc05123db731142d54f89c4b6eda3bff56c1
antoniopd1 <antoniocan491@gmail.com>
2024-07-31 11:46:34 -0600

    Practica #2 Express

--- practica1-backend/myapp/app.js
+++ practica1-backend/myapp/app.js
@@ -25,3 +25,19 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3002; 
+
+// app.get('/hello', (req, res) => {
+//   const { name, lastName } = req.query;
+//   res.send(`Hola ${name} ${lastName}!`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
